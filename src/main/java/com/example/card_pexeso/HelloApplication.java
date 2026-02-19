b073504348dc28c0eb8f299c694bc20b64c05296
drtluk00 <drtluk00@gasos-ro.cz>
2026-02-19 11:05:51 +0100

    HEAAALYEAAAAA

--- src/main/java/com/example/card_pexeso/HelloApplication.java
+++ src/main/java/com/example/card_pexeso/HelloApplication.java
@@ -11,7 +11,8 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 610, 900);
+        Scene scene = new Scene(fxmlLoader.load(), 690, 990);
+        scene.getStylesheets().add(HelloApplication.class.getResource("style.css").toExternalForm());
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
